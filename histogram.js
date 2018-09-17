7876dba7d7c6b79e8de8dc5d29221b486a5316bd
Champe Barton <champebarton@gmail.com>
2018-09-17 00:06:55 -0400

    Update histogram.js

--- histogram.js
+++ histogram.js
@@ -187,7 +187,7 @@ function drawHistogram(){
     });
 
     var values = [];
-    var nonWhiteColor = "#000000" // var nonWhiteColor = "#E0451F" nonWhiteColor = "#FA0E4F"
+    var nonWhiteColor = "#3ecdfd" // var nonWhiteColor = "#E0451F" nonWhiteColor = "#FA0E4F"
     var whiteColor = "#fecb45" //var whiteColor = "#2161FA" #18638B var whiteColor = "#249EFB" //var whiteColor = "#2161FA"
 
     movieData = scriptNest;
@@ -4281,7 +4281,7 @@ function drawHistogram(){
   }
 
   if(mobile) {
-      grossChart.attr("webkit-transform", "translate(" + 25.5 + "," + 10 + ")");
+      grossChart.attr("webkit-transform", "translate(" + 33.5 + "," + 100 + ")");
 //       grossChart.attr("ms-transform", "translate(" + 33.5 + "," + 4 + ")");
 //       grossChart.attr("moz-transform", "translate(" + 33.5 + "," + 4 + ")");
 //       grossChart.attr("transform", "translate(" + 33.5 + "," + 4 + ")");
@@ -4497,8 +4497,8 @@ function drawHistogram(){
       xShift = Math.floor((window.innerWidth - testWidth) / 2);
       yShift = -30;
     } else {
-      xShift = Math.floor((window.innerWidth - testWidth) / 2) - 30;
-      yShift = -20;
+      xShift = Math.floor((window.innerWidth - testWidth) / 2) - 10;
+      yShift = -30;
     }
 
     // if(mobile) {
